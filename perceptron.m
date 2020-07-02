4b99190c4d6f06fceabb509af9f37c602dbefb5f
Aurelio Siordia <lz_rock@hotmail.com>
2020-07-01 21:30:47 -0500

    perceptron entrenado y probado con datos aleatorios

--- perceptron.m
+++ perceptron.m
@@ -1,11 +1,11 @@
-x = [      %Entradas
+x = [      %Entradas de entrenamiento
     -1 -1;
     -1  1;
      1 -1;
      1  1; 
      ];
 
- y = [     %Salida
+ y = [     %Salida de entrenamiento
        -1  
        -1
        -1
@@ -20,7 +20,7 @@ beta   = 1;
 answer = 0;
 
 
-%%%% Algoritmo de entrenamineto del perceptron
+%%%% Algoritmo de entrenamineto del perceptron %%%%
 for i = 1:l
  
  %Calcular entrada ponderada   
@@ -45,8 +45,45 @@ for i = 1:l
  %calcular nuevo vector ponderaciones
   for j = 1:m
     w(j) = w(j) + (beta * answer * x(i, j));
- end
- 
+  end
  I = 0;
 end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+
+
+%%%% Algoritmo de implementacion del perceptron %%%%
+
+ %Generando datos aleatorios 
+r = round( -1 + (1+1)*rand(20,3) );
+
+[o,p] = size(r); % o: numero de filas  
+                 % p: numero de columnas 
+                 p = p-1;
+
+for i = 1:o
+ %Calcular entrada ponderada 
+ for j = 1:p
+ 
+ %Ajuste para valores 0 en los dataos generados aleatoriamente
+ if r(i,j) == 0   
+  r(i,j) = -1;   
+ end
+     
+     
+    I = I + ( w(j) * r(i,j) ); 
+ end
+
+  %Calcular la respuesta
+ if I >= theta
+  r(i,3) = 1;  
+  row =   num2str(i); 
+  text = strcat('patron reconocido en: ', row );   
+  fprintf(text);
+  fprintf('\n');
+ elseif I < theta
+  r(i,3) = -1;   
+ end
+  I = 0;
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
